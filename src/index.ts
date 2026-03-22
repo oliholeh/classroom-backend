d4c0336b78e6e26f5ec84baf49cb4098f08de30e
oleh.olikh <Oleh.Olikh@pixelsmatter.com>
2026-03-22 16:38:49 +0200

    initial commit

---
+++ src/index.ts
@@ -0,0 +1,17 @@
+import express from "express";
+
+const app = express();
+const PORT = 8000;
+
+// Middleware
+app.use(express.json());
+
+// Routes
+app.get("/", (req, res) => {
+  res.json({ message: "Welcome to the Classroom API" });
+});
+
+// Start server
+app.listen(PORT, () => {
+  console.log(`Server is running at http://localhost:${PORT}`);
+});
