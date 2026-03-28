682062c03974217ec3b3709ea0fcdf3798a9cccf
oleh.olikh <Oleh.Olikh@pixelsmatter.com>
2026-03-28 19:11:20 +0200

    Implement get all subjects route

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,19 @@
 import express from "express";
+import subjectsRouter from "./routes/subjects";
+import cors from "cors";
 
 const app = express();
 const PORT = 8000;
 
 // Middleware
+app.use(cors({
+  origin: process.env.FRONTEND_URL || "http://localhost:5173",
+  methods: ['GET', 'POST', 'PUT', 'DELETE'],
+  credentials: true,
+}))
 app.use(express.json());
+
+app.use("/api/subjects", subjectsRouter);
 
 // Routes
 app.get("/", (req, res) => {
